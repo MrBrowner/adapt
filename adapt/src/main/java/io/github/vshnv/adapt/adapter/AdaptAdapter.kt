a777b8af6aeda5409b46ca9c9b0e2c31f8a7db35
browner_zo_pc <24374030+MrBrowner@users.noreply.github.com>
2024-07-12 11:32:46 +0530

    feature: filterable for adapter

--- adapt/src/main/java/io/github/vshnv/adapt/adapter/AdaptAdapter.kt
+++ adapt/src/main/java/io/github/vshnv/adapt/adapter/AdaptAdapter.kt
@@ -1,11 +1,15 @@
 package io.github.vshnv.adapt.adapter
 
+import android.widget.Filter
+import android.widget.Filterable
 import androidx.recyclerview.widget.RecyclerView
 
-abstract class AdaptAdapter<T>: RecyclerView.Adapter<AdaptViewHolder<T>>() {
+abstract class AdaptAdapter<T>: RecyclerView.Adapter<AdaptViewHolder<T>>(), Filterable {
     abstract val currentList: List<T>
+    abstract var searchFilterable: Filter?
+    abstract fun getFullData(): List<T>
 
     abstract suspend fun submitDataSuspending(data: List<T>): Unit
-
     abstract fun submitData(data: List<T>, callback: () -> Unit = {}): Unit
+    abstract fun submitDataFromFilter(data: List<T>, callback: () -> Unit = {}): Unit
 }
--- adapt/src/main/java/io/github/vshnv/adapt/adapter/LifecycleAwareAdaptAdapter.kt
+++ adapt/src/main/java/io/github/vshnv/adapt/adapter/LifecycleAwareAdaptAdapter.kt
@@ -2,6 +2,7 @@ package io.github.vshnv.adapt.adapter
 
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.AsyncListDiffer
@@ -28,12 +29,22 @@ class LifecycleAwareAdaptAdapter<T : Any>(private val viewTypeMapper: ((T, Int) 
     private val mDiffer: AsyncListDiffer<T> = AsyncListDiffer(this, diffCallback)
     override val currentList: List<T>
         get() = mDiffer.currentList
+    private var fullList: MutableList<T> = mutableListOf()
+
+    override var searchFilterable: Filter? = null
+
+    override fun getFilter(): Filter = requireNotNull(searchFilterable) {
+        "Filterable.Filter of $this accessed before assigning"
+    }
+
+    override fun getFullData(): List<T> = fullList
 
     override fun getItemViewType(position: Int): Int {
         return viewTypeMapper?.let {
             it(getItem(position), position)
         } ?: super.getItemViewType(position)
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdaptViewHolder<T> {
         val binderItem: CollectingBindable<T, *> = viewBinders[viewType] ?: defaultBinder
         ?: throw AssertionError("Adapt found ViewType with no bound view creator or any default view creator, Cannot proceed!")
@@ -61,13 +72,21 @@ class LifecycleAwareAdaptAdapter<T : Any>(private val viewTypeMapper: ((T, Int) 
         return mDiffer.currentList[position]
     }
 
-    override suspend fun submitDataSuspending(data: List<T>) = suspendCoroutine<Unit> { continuation ->
-        mDiffer.submitList(data) {
-            continuation.resumeWith(Result.success(Unit))
+    override suspend fun submitDataSuspending(data: List<T>) {
+        fullList = data.toMutableList()
+        suspendCoroutine<Unit> { continuation ->
+            mDiffer.submitList(data) {
+                continuation.resumeWith(Result.success(Unit))
+            }
         }
     }
 
     override fun submitData(data: List<T>, callback: () -> Unit) {
+        fullList = data.toMutableList()
+        mDiffer.submitList(data, callback)
+    }
+
+    override fun submitDataFromFilter(data: List<T>, callback: () -> Unit) {
         mDiffer.submitList(data, callback)
     }
 
@@ -87,6 +106,7 @@ class LifecycleAwareAdaptAdapter<T : Any>(private val viewTypeMapper: ((T, Int) 
         registry?.highestState = Lifecycle.State.RESUMED
         knownAffectedViewHolders.add(holder)
     }
+
     override fun onViewDetachedFromWindow(holder: AdaptViewHolder<T>) {
         val registry = (holder as LifecycleAwareAdaptViewHolder<T>).lifecycleRegistry
         registry?.highestState = Lifecycle.State.CREATED
